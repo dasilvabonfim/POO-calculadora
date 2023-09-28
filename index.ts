e38124c80694648c65479a4585498ca4e5b39fe5
dasilvabonfim <112987011+dasilvabonfim@users.noreply.github.com>
2023-09-28 15:17:24 -0300

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,11 @@
+import { Calculator } from "./calculator";
+
+const calculator = new Calculator();
+calculator.pressButton("2");
+calculator.pressButton("+");
+calculator.pressButton("2");
+calculator.pressButton("2");
+calculator.pressButton("/");
+calculator.pressButton("1");
+calculator.pressButton("0");
+calculator.pressButton("=");
